3cc69c8303fb46cef835f484f90759d816350428
Praveen Kumar <95642081+praveen-9721@users.noreply.github.com>
2025-07-06 14:40:05 +0530

    feat: add SpaceX feature

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+// Created a new React component for the SpaceX Dashboard
+// This component fetches and displays SpaceX launches, allowing users to filter by upcoming or past
+
 import React from 'react';
 import Dashboard from './components/Pages/Dashboard';
 // import Dashboard from './Dashboard'; // Adjust the path if Dashboard is in a subfolder
